03224b72c6294d1a7579b423d86138efd2974dfe
VITOR ROCHA MARQUES <01711559040@senacrs.edu.br>
2026-06-16 10:36:18 -0300

    arquivo inicial

---
+++ p.py
@@ -0,0 +1,30 @@
+import json
+
+
+oc = []
+
+
+
+
+
+
+while True:
+    print("MENU")
+    print("1. Cadastrar ocorrencia")
+    print("2. Listar todas as ocorrencias")
+
+    op = input("selecione a opcao: ")
+
+    if op == "1":
+        i = input("cadastrar? ")
+        if i == "sim":
+            ocorrencia = input("digite a ocorrencia: ")
+            oc.append(ocorrencia)
+            print("Ocorrencia inserida!")
+        else:
+            break
+    if op == "2":
+        print(oc)
+
+
+
